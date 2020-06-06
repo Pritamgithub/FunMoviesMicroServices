6236bfb0f9a6c2f37558fe195221203b8c9b470b
Pritam Chavhan <pritam0263@gmail.com>
2020-06-06 01:11:31 -0400

    Fixed local errors for MovieInfoMicroservice

--- MovieInfoMicroService/src/main/java/com/movie/controller/MovieResource.java
+++ MovieInfoMicroService/src/main/java/com/movie/controller/MovieResource.java
@@ -1,22 +1,32 @@
 package com.movie.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.movie.models.Movie;
+import com.movie.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
+	@Autowired
+	RestTemplate restTemplate;
+	@Value("${api.key}")
+	private String apiKey;
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		
-		
-		return new Movie(movieId, "Test");
-		
-		
+
+		MovieSummary movieSummary = restTemplate
+				.getForObject("https://api.themoviedb.ord/3/movie" + movieId
+						+ "?api_key=" + apiKey, MovieSummary.class);
+		return new Movie(movieId, movieSummary.getTitle(),
+				movieSummary.getOverview());
+
 	}
 
 }
---
+++ MovieInfoMicroService/src/main/java/com/movie/models/MovieSummary.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.movie.models;
+
+import lombok.Data;
+
+/**
+ * @author Pritam Chavhan
+ *
+ */
+@Data
+public class MovieSummary {
+
+	private String id;
+	private String title;
+	private String overview;
+	
+}
