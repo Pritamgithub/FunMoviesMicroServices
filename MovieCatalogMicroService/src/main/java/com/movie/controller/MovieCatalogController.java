229569b6ca460c14c9bb72cf2462fac8817ff4a4
Pritam Chavhan <pritam0263@gmail.com>
2020-06-06 21:44:51 -0400

    Added more functionality
    +Added Hystrix for Fault tolerance and resilence as a circuit break solution for slow microservices another solution than using Timeouts
    +Bulkhead Pattern

--- MovieCatalogMicroService/src/main/java/com/movie/controller/MovieCatalogController.java
+++ MovieCatalogMicroService/src/main/java/com/movie/controller/MovieCatalogController.java
@@ -15,6 +15,7 @@ import com.movie.models.CatalogItem;
 import com.movie.models.Movie;
 import com.movie.models.Rating;
 import com.movie.models.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/catalog")
@@ -24,23 +25,40 @@ public class MovieCatalogController {
 	private RestTemplate restTemplate;
 
 	@RequestMapping("/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalog")
 	public List<CatalogItem> getCatalog(@PathVariable("userid") String userId)
 	{ 
 		
-		UserRating ratings=restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" +userId, UserRating.class);
+		UserRating ratings=getForObject(userId);
 	
 		
 		// for each movie ID, call movie info service and get details
 		return ratings.getUserRating().stream().map(rating->{
 			
-			Movie movie=restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
+			Movie movie=getCatalogItem(rating);
 			//Put all them together
-			return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+			return getCatalogItem(rating, movie);
 			})
 				.collect(Collectors.toList());
 		//get all rated movie Ids
 		//return Collections.singletonList(new CatalogItem(name:"Transformers", desc:"Test", rating:4));
 		
 	}
+	/*
+	 * // public List<CatalogItem> getFallbackCatalog(@PathVariable("userid")
+	 * String userId){ // return Arrays.asList(new CatalogItem("No movie", "",""
+	 * )); // // // }
+	 */
 
+	private CatalogItem getCatalogItem(Rating rating, Movie movie) {
+		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+	}
+
+	private Movie getCatalogItem(Rating rating) {
+		return restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
+	}
+
+	private UserRating getForObject(String userId) {
+		return restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" +userId, UserRating.class);
+	}
 }
--- MovieCatalogMicroService/src/main/java/com/movie/main/MovieCatalogMicroServiceApplication.java
+++ MovieCatalogMicroService/src/main/java/com/movie/main/MovieCatalogMicroServiceApplication.java
@@ -2,26 +2,33 @@ package com.movie.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MovieCatalogMicroServiceApplication {
 
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		// TODO
+		// set timeouts to overcome the slow processing of the microservices
+		// solution for this is to set Timeout at RestTemplate
+
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);//Partial solution to remove the slowness of a microservice
+		return new RestTemplate(clientHttpRequestFactory);
 
 	}
-	
+
 	/*
 	 * @Bean public WebClient.Builder getWebClientBuilder() { return new
 	 * WebClient.builder();
 	 * 
 	 * }
 	 */
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogMicroServiceApplication.class, args);
